3b4259bf9269e372108071120fefaf8bd8b82f8b
Mahmoud Hisham <mahmoudhisham11>
2026-05-25 14:46:08 +0300

    add new updates

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-});
+};
+
+module.exports = withPWA(nextConfig);
